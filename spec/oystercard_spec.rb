ad832a06008f604c835477df44f2ffcf8d9c838c
Student <student@Makerss-MacBook-Pro-5.local>
2019-12-17 14:49:03 +0000

    adding top up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,16 @@ describe Oystercard do
         expect(subject.balance).to eq(0)
     end
   end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+
+    it 'has a limit of 90' do
+      max
+  end
+
 end
