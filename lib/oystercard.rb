fbb8b2f7899acb93773305ebd0290178704d43b9
Student <student@Makerss-MacBook-Pro-5.local>
2019-12-17 15:47:56 +0000

    added deduct method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -14,4 +14,7 @@ class Oystercard
     @balance += amount
   end
 
+  def deduct(amount)
+    @balance -= amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,7 +9,6 @@ describe Oystercard do
   end
 
   describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'can top up balance' do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
@@ -19,6 +18,14 @@ describe Oystercard do
       max_balance = Oystercard::MAX_BALANCE
       subject.top_up(max_balance)
       expect { subject.top_up 1 }.to raise_error "Balance cannot exceed £#{max_balance}"
+    end
+  end
+
+  describe "#deduct" do
+    it "deducts from balance" do
+      subject.top_up(1)
+      subject.deduct(1)
+      expect(subject.balance).to eq 0
     end
   end
 end
